14d59c3c01a14a8bf63e304a79be8dcfa0966b08
Syed Shiraz Ahmed <syedshiraz_october@yahoo.co.in>
2019-01-19 22:54:38 +0530

    Adding the initial code

---
+++ Fresco/TheSBrowser.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Fresco
+{
+    partial class TheSBrowser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnNavigate = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Browser = new System.Windows.Forms.WebBrowser();
+            this.btn_InjectJS = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Controls.Add(this.btnNavigate);
+            this.panel1.Controls.Add(this.txtUrl);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(958, 35);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL:";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUrl.Location = new System.Drawing.Point(58, 5);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(755, 22);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // btnNavigate
+            // 
+            this.btnNavigate.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnNavigate.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnNavigate.Location = new System.Drawing.Point(819, 0);
+            this.btnNavigate.Name = "btnNavigate";
+            this.btnNavigate.Size = new System.Drawing.Size(139, 35);
+            this.btnNavigate.TabIndex = 2;
+            this.btnNavigate.Text = "Navigate";
+            this.btnNavigate.UseVisualStyleBackColor = true;
+            this.btnNavigate.Click += new System.EventHandler(this.btnNavigate_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel2.Controls.Add(this.btn_InjectJS);
+            this.panel2.Location = new System.Drawing.Point(0, 38);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(958, 32);
+            this.panel2.TabIndex = 1;
+            // 
+            // Browser
+            // 
+            this.Browser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Browser.Location = new System.Drawing.Point(0, 76);
+            this.Browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.Browser.Name = "Browser";
+            this.Browser.Size = new System.Drawing.Size(958, 429);
+            this.Browser.TabIndex = 2;
+            // 
+            // btn_InjectJS
+            // 
+            this.btn_InjectJS.Location = new System.Drawing.Point(212, 3);
+            this.btn_InjectJS.Name = "btn_InjectJS";
+            this.btn_InjectJS.Size = new System.Drawing.Size(152, 25);
+            this.btn_InjectJS.TabIndex = 0;
+            this.btn_InjectJS.Text = "Inject The Magic";
+            this.btn_InjectJS.UseVisualStyleBackColor = true;
+            this.btn_InjectJS.Click += new System.EventHandler(this.btn_InjectJS_Click);
+            // 
+            // TheSBrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(961, 508);
+            this.Controls.Add(this.Browser);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "TheSBrowser";
+            this.Text = "TheSBrowser";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnNavigate;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.WebBrowser Browser;
+        private System.Windows.Forms.Button btn_InjectJS;
+    }
+}
+
---
+++ Fresco/TheSBrowser.cs
@@ -0,0 +1,96 @@
+﻿using MSHTML;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fresco
+{
+    public partial class TheSBrowser : Form
+    {
+        public TheSBrowser()
+        {
+            InitializeComponent();
+            Browser.ObjectForScripting = true;
+            try
+            {
+                int versionCode=7000;
+                String version = "";
+                String appName = System.Reflection.Assembly.GetExecutingAssembly().FullName;
+                object ieVersion = Microsoft.Win32.Registry.LocalMachine.OpenSubKey("Software\\Microsoft\\Internet Explorer").GetValue("svcUpdateVersion");
+                if (ieVersion == null)
+                    ieVersion = Microsoft.Win32.Registry.LocalMachine.OpenSubKey("Software\\Microsoft\\Internet Explorer").GetValue("Version");
+                else
+                {
+                    version = ieVersion.ToString().Substring(0, ieVersion.ToString().IndexOf("."));
+                    Console.WriteLine("Current Version of IE : " + version);
+                    switch (version)
+                    {
+                        case "7":
+                            versionCode = 7000;
+                            break;
+                        case "8":
+                            versionCode = 8888;
+                            break;
+                        case "9":
+                            versionCode = 9999;
+                            break;
+                        case "10":
+                            versionCode = 10001;
+                            break;
+                        default:
+                            if (int.Parse(version) >= 11)
+                                versionCode = 11001;
+                            else
+                                throw new Exception("IE Version Not Supported");
+                            break;
+                    }
+                }
+                Console.WriteLine("Set Version Code : " + versionCode);
+                String root = "HKEY_CURRENT_USER\\";
+                String key = "Software\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION";
+                object currentSetting = (Microsoft.Win32.Registry.CurrentUser.OpenSubKey(key).GetValue(appName + ".exe"));
+                if ((currentSetting == null) || ((int.Parse(currentSetting.ToString())) != versionCode))
+                {
+                    Microsoft.Win32.Registry.SetValue(root + key, appName + ".exe", versionCode);
+                    Microsoft.Win32.Registry.SetValue(root + key, appName + ".vshost.exe", versionCode);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+
+        private void Navigate()
+        {
+            String url = txtUrl.Text;
+            Browser.Navigate(url);
+        }
+
+        private void InjectJavaScript()
+        {
+            HtmlElement head = Browser.Document.GetElementsByTagName("head")[0];
+            HtmlElement script = Browser.Document.CreateElement("script");
+            IHTMLScriptElement element = (IHTMLScriptElement)script.DomElement;
+            element.text = "function hello() {alert(\"Yeah boy you did it\")}";
+            head.AppendChild(script);
+            Browser.Document.InvokeScript("hello");
+        }
+
+        private void btnNavigate_Click(object sender, EventArgs e)
+        {
+            Navigate();
+        }
+
+        private void btn_InjectJS_Click(object sender, EventArgs e)
+        {
+            InjectJavaScript();
+        }
+    }
+}
